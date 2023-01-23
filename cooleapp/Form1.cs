d234d98497f1180eb49c3c4e7ca03b131b492489
finleysiebert <finleysiebert@FINLEYSIEBEB8B5>
2023-01-23 10:00:47 +0100

    Add project files.

---
+++ cooleapp/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cooleapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Controls.Add(new Startscherm());
+       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("De game gaat nu beginnen!");
+            panel1.Controls.Clear();
+            panel1.Controls.Add(new Gamevenster());
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+    }
+}
---
+++ cooleapp/Startscherm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace cooleapp
+{
+    partial class Startscherm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(323, 342);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(491, 82);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mijn mooie game";
+            // 
+            // Startscherm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "Startscherm";
+            this.Size = new System.Drawing.Size(1175, 807);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
